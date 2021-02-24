fe20bc2a2408bbaa9a7ed684c0c80160a937f34d
Bilal-Yucell <bilallyucell@gmail.com>
2021-02-24 19:29:18 +0300

    Add project files.

---
+++ Business/Abstract/ICarService.cs
@@ -0,0 +1,22 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ICarService
+    {
+        IDataResult<List<Car>> GetAll();
+        IDataResult<List<Car>> GetCarsByBrandId(int id);
+        IDataResult<List<Car>> GetCarsByColorId(int id);
+        IDataResult<List<CarDetailDto>> GetCarDetails(Expression<Func<Car, bool>> filter = null);
+        IResult Add(Car car);
+        IResult Update(Car car);
+        IResult Delete(Car car);
+        IDataResult<Car> GetById(int carId);
+    }
+}
